4ff5b3eaec29289075c655bb0d4b2e36ab3dc251
Palaksh <167078995+Palaksh-singh@users.noreply.github.com>
2026-07-18 00:31:54 +0530

    Time: 20 ms (65.92%), Space: 45.5 MB (95.05%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,22 +1,33 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
+        Arrays.sort(nums);
         for (int i = 0; i<n; i++) {
-            for (int j = i + 1; j<n; j++) {
-                HashSet<Integer> seen = new HashSet<>();
-                for (int k = j + 1; k<n; k++) {
-                    long required = (long)target - (long)nums[i] - (long)nums[j] - (long)nums[k];
-                    if (required >= Integer.MIN_VALUE && required <= Integer.MAX_VALUE && seen.contains((int) required)) {
-                        List<Integer> ans = Arrays.asList(nums[i], nums[j], nums[k], (int)required);
-                        Collections.sort(ans);
-                        set.add(ans);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j <n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1 , right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[left] + (long)nums[right];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
                     }
-                    seen.add(nums[k]);
+                    else if (sum < target) left++;
+                    else right--;
                 }
             }
         }
-        return new ArrayList<>(set);
+        return ans;
     }
 }
