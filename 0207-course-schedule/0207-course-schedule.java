7275b3a7b5140cb94938a49fe0df174410c4517f
Palaksh <167078995+Palaksh-singh@users.noreply.github.com>
2026-07-18 14:35:49 +0530

    Time: 6 ms (72.2%), Space: 47.5 MB (8.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,41 @@
-// numCourses = 6
-// prerequisites = [
-//   [2, 1],  // To take 2, finish 1
-//   [3, 2],  // To take 3, finish 2
-//   [4, 3],  // To take 4, finish 3
-//   [5, 4],  // To take 5, finish 4
-//   [3, 0]   // To take 3, finish 0
-// ]
-
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++)
-          adj.add(new ArrayList<Integer>());
 
-        int[] inDegree=new int[numCourses];
-        Queue<Integer> q=new LinkedList<>();
-
-        for(int[] entireRow:prerequisites)
-        {
-            int course=entireRow[0];
-            int neededCourse=entireRow[1];
-            adj.get(neededCourse).add(course);
-            inDegree[course]++;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<numCourses;i++)
-          if(inDegree[i]==0)
-             q.add(i);
+        int m = prerequisites.length;
+        for (int i = 0; i<m; i++) {
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
 
-        List<Integer> res=new ArrayList<>(); 
-
-        while(!q.isEmpty())
-        {
-            int completedCourse=q.poll();
-            res.add(completedCourse);
-            for(int nextCouse:adj.get(completedCourse))
-            {
-                inDegree[nextCouse]--;
-                if(inDegree[nextCouse]==0)
-                  q.add(nextCouse);
+        int indegree[] = new int[numCourses];
+        for (int i = 0; i<numCourses; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
             }
         }
-        
-        return numCourses==res.size();
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i<numCourses; i++) {
+            if (indegree[i] == 0) q.add(i);
+        }
+
+        List<Integer> topo = new ArrayList<Integer>();
+
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo.add(node);
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+        if (topo.size() == numCourses) return true;
+        return false;
     }
 }
