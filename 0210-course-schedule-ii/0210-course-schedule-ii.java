3819a38397096dc747edf40c605d7ec6aa580824
Palaksh <167078995+Palaksh-singh@users.noreply.github.com>
2026-07-18 14:52:24 +0530

    Time: 6 ms (43.52%), Space: 46.7 MB (67.31%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,70 +1,40 @@
 class Solution {
-    static class DetectCycleGraph {
-        private int V;
-        private List<List<Integer>> edges;
-
-        DetectCycleGraph(int n) {
-            this.V = n;
-            System.out.println(n);
-            edges = new ArrayList<>();
-
-            for (int i = 0; i < n; i++) {
-                edges.add(new ArrayList<>());
-            }
-        }
-
-        public DetectCycleGraph() {
-        }
-
-        private void addEdge(int i, int j) {
-            edges.get(i).add(j);
-        }
-
-        List<Integer> l = new ArrayList<>();
-
-        public int[] isCycle(DetectCycleGraph graph) {
-            boolean[] vis = new boolean[this.V];
-            boolean[] curRec = new boolean[this.V];
-            for(int i = 0; i < this.V; i++) {
-                if (isCycleUtil(vis, curRec, i)) return new int[0];
-            }
-
-            int[] res = new int[V];
-            int index = 0;
-            for (Integer i : l) {
-                res[index] = i;
-                index++;
-            }
-            return res;
-        }
-
-        public boolean isCycleUtil(boolean[] vis, boolean[] curRec, int i) {
-            if (vis[i] == false) {
-                vis[i] = true;
-                curRec[i] = true;
-
-                List<Integer> neighbors = edges.get(i);
-                for (Integer v : neighbors) {
-
-                    if (!vis[v] && isCycleUtil(vis, curRec, v)) return true;
-                    else if (curRec[v]) return true;
-                }
-                l.add(new Integer(i));
-            }
-
-            curRec[i] = false;
-
-            return false;
-        }
-    }
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
-        DetectCycleGraph graph = new DetectCycleGraph(numCourses);
-        for (int i = 0; i < prerequisites.length; i++) {
-            int[] sub = prerequisites[i];
-            graph.addEdge(sub[0], sub[1]);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
         }
-        return graph.isCycle(graph);
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int indegree[] = new int[numCourses];
+        for (int i = 0; i<numCourses; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i<numCourses; i++) {
+            if (indegree[i] == 0) q.add(i);
+        }
+
+        int[] topo = new int[numCourses];
+        int index = 0;
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            topo[index++] = node;
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+        if (index == topo.length) return topo;
+        int[] arr = {};
+        return arr;
     }
 }
