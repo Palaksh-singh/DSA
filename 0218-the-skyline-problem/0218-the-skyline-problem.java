2ff0e84c0d5f51cdf408b5d8c2462e88e5d1d1cf
Palaksh <167078995+Palaksh-singh@users.noreply.github.com>
2026-08-17 01:43:37 +0530

    Time: 33 ms (62.15%), Space: 53.4 MB (82.47%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -5,56 +5,62 @@ class Solution {
 
         // Create start and end events
         for (int[] b : buildings) {
-            int left = b[0];
-            int right = b[1];
-            int height = b[2];
-
-            // Start event
-            events.add(new int[]{left, -height});
-            // End event
-            events.add(new int[]{right, height});
+            events.add(new int[]{b[0], -b[2]}); // start
+            events.add(new int[]{b[1], b[2]});  // end
         }
 
-        // Sort by x-coordinate.
-        // If x is same, sort by height.
+        // Sort by x, then height
         Collections.sort(events, (a, b) -> {
             if (a[0] != b[0]) {
                 return Integer.compare(a[0], b[0]);
             }
-
             return Integer.compare(a[1], b[1]);
         });
 
-        // Max heap containing active building heights
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        pq.add(0);
+        // TreeMap acts like a multiset:
+        // key   = height
+        // value = number of buildings having that height
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        // Ground level
+        map.put(0, 1);
 
         List<List<Integer>> result = new ArrayList<>();
+
         int prevHeight = 0;
 
         for (int[] event : events) {
+
             int x = event[0];
             int h = event[1];
 
-            // Building starts
             if (h < 0) {
-                pq.add(-h);
+                // Building starts
+                h = -h;
+
+                map.put(h, map.getOrDefault(h, 0) + 1);
+
+            } else {
+                // Building ends
+                int count = map.get(h);
+
+                if (count == 1) {
+                    map.remove(h);
+                } else {
+                    map.put(h, count - 1);
+                }
             }
 
-            // Building ends
-            else {
-                pq.remove(h);
-            }
-
-            // Current tallest building
-            int currHeight = pq.peek();
+            // Highest active building
+            int currHeight = map.lastKey();
 
             // Skyline changed
             if (currHeight != prevHeight) {
-                result.add( Arrays.asList(x, currHeight) );
+                result.add(Arrays.asList(x, currHeight));
                 prevHeight = currHeight;
             }
         }
+
         return result;
     }
 }
